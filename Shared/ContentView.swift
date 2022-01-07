08b19b8b4fa23e3dfc66f37ce4217907a4f8a5f0
renato <felicio95.jr@gmail.com>
2022-01-07 16:58:48 -0300

    :tada: Adding `HelloWorld` project
    At the moment, adds a single-vew app. This app displays Hello World and a buttom below it.

---
+++ Shared/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Renato F. dos Santos Jr on 06/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .padding()
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  Shared
+//
+//  Created by Renato F. dos Santos Jr on 06/01/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
